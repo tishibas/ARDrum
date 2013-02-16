9910ec905a0ed297310b242114636670dd3d8297
T.I <t_ishibas67@yahoo.co.jp>
2013-02-16 19:23:17 +0900

    opencv244

--- src/actiondetect/common.h
+++ src/actiondetect/common.h
@@ -1,44 +1,52 @@
 #define _USE_MATH_DEFINES
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
 #include <math.h>
-#include <Windows.h>
 #include <vector>
+#include <Windows.h>
+#include <time.h>
 
-#include "opencv2\opencv.hpp"
+#include <opencv2/opencv.hpp>
+#include <glut.h>
+#include <MIDIIO.h>
 #ifdef _DEBUG 
-//Debugモードの場合
-#pragma comment(lib,"C:\\work\\cv\\opencv\\build\\x86\\vc10\\lib\\opencv_core240d.lib")    
-#pragma comment(lib,"C:\\work\\cv\\opencv\\build\\x86\\vc10\\lib\\opencv_imgproc240d.lib")  
-#pragma comment(lib,"C:\\work\\cv\\opencv\\build\\x86\\vc10\\lib\\opencv_highgui240d.lib")  
-#pragma comment(lib,"C:\\work\\cv\\opencv\\build\\x86\\vc10\\lib\\opencv_objdetect240d.lib")
-#pragma comment(lib,"C:\\work\\cv\\opencv\\build\\x86\\vc10\\lib\\opencv_contrib240d.lib")   
-#pragma comment(lib,"C:\\work\\cv\\opencv\\build\\x86\\vc10\\lib\\opencv_features2d240d.lib")
-#pragma comment(lib,"C:\\work\\cv\\opencv\\build\\x86\\vc10\\lib\\opencv_flann240d.lib")   
-#pragma comment(lib,"C:\\work\\cv\\opencv\\build\\x86\\vc10\\lib\\opencv_gpu240d.lib")   
-#pragma comment(lib,"C:\\work\\cv\\opencv\\build\\x86\\vc10\\lib\\opencv_haartraining_engined.lib") 
-#pragma comment(lib,"C:\\work\\cv\\opencv\\build\\x86\\vc10\\lib\\opencv_legacy240d.lib")  
-#pragma comment(lib,"C:\\work\\cv\\opencv\\build\\x86\\vc10\\lib\\opencv_ts240d.lib")  
-#pragma comment(lib,"C:\\work\\cv\\opencv\\build\\x86\\vc10\\lib\\opencv_video240d.lib")
-#pragma comment(lib,"C:\\work\\cv\\opencv\\build\\x86\\vc10\\lib\\opencv_calib3d240d.lib")
+//Debugモードの場合private\\program\\opencv
+#pragma comment(lib,"C:\\private\\program\\opencv\\opencv\\build\\x86\\vc10\\lib\\opencv_core244d.lib")    
+#pragma comment(lib,"C:\\private\\program\\opencv\\opencv\\build\\x86\\vc10\\lib\\opencv_imgproc244d.lib")  
+#pragma comment(lib,"C:\\private\\program\\opencv\\opencv\\build\\x86\\vc10\\lib\\opencv_highgui244d.lib")  
+#pragma comment(lib,"C:\\private\\program\\opencv\\opencv\\build\\x86\\vc10\\lib\\opencv_objdetect244d.lib")
+#pragma comment(lib,"C:\\private\\program\\opencv\\opencv\\build\\x86\\vc10\\lib\\opencv_contrib244d.lib")   
+#pragma comment(lib,"C:\\private\\program\\opencv\\opencv\\build\\x86\\vc10\\lib\\opencv_features2d244d.lib")
+#pragma comment(lib,"C:\\private\\program\\opencv\\opencv\\build\\x86\\vc10\\lib\\opencv_flann244d.lib")   
+#pragma comment(lib,"C:\\private\\program\\opencv\\opencv\\build\\x86\\vc10\\lib\\opencv_gpu244d.lib")   
+#pragma comment(lib,"C:\\private\\program\\opencv\\opencv\\build\\x86\\vc10\\lib\\opencv_haartraining_engined.lib") 
+#pragma comment(lib,"C:\\private\\program\\opencv\\opencv\\build\\x86\\vc10\\lib\\opencv_legacy244d.lib")  
+#pragma comment(lib,"C:\\private\\program\\opencv\\opencv\\build\\x86\\vc10\\lib\\opencv_ts244d.lib")  
+#pragma comment(lib,"C:\\private\\program\\opencv\\opencv\\build\\x86\\vc10\\lib\\opencv_video244d.lib")
+#pragma comment(lib,"C:\\private\\program\\opencv\\opencv\\build\\x86\\vc10\\lib\\opencv_calib3d244d.lib")
+#pragma comment(lib,"C:\\private\\program\\midi\\MIDIIOLib0.8\\Debug\\MIDIIOd.lib")
 #else    //Releaseモードの場合  
-#pragma comment(lib,"C:\\work\\cv\\opencv\\build\\x86\\vc10\\lib\\opencv_core240.lib")  
-#pragma comment(lib,"C:\\work\\cv\\opencv\\build\\x86\\vc10\\lib\\opencv_imgproc240.lib")  
-#pragma comment(lib,"C:\\work\\cv\\opencv\\build\\x86\\vc10\\lib\\opencv_highgui240.lib")  
-#pragma comment(lib,"C:\\work\\cv\\opencv\\build\\x86\\vc10\\lib\\opencv_objdetect240.lib")
-#pragma comment(lib,"C:\\work\\cv\\opencv\\build\\x86\\vc10\\lib\\opencv_contrib240.lib")  
-#pragma comment(lib,"C:\\work\\cv\\opencv\\build\\x86\\vc10\\lib\\opencv_features2d240.lib") 
-#pragma comment(lib,"C:\\work\\cv\\opencv\\build\\x86\\vc10\\lib\\opencv_flann240.lib")  
-#pragma comment(lib,"C:\\work\\cv\\opencv\\build\\x86\\vc10\\lib\\opencv_gpu240.lib")   
-#pragma comment(lib,"C:\\work\\cv\\opencv\\build\\x86\\vc10\\lib\\opencv_haartraining_engined.lib")  
-#pragma comment(lib,"C:\\work\\cv\\opencv\\build\\x86\\vc10\\lib\\opencv_legacy240.lib")   
-#pragma comment(lib,"C:\\work\\cv\\opencv\\build\\x86\\vc10\\lib\\opencv_ts240.lib")   
-#pragma comment(lib,"C:\\work\\cv\\opencv\\build\\x86\\vc10\\lib\\opencv_video240.lib")
-#pragma comment(lib,"C:\\work\\cv\\opencv\\build\\x86\\vc10\\lib\\opencv_calib3d240.lib")
+#pragma comment(lib,"C:\\private\\program\\opencv\\opencv\\build\\x86\\vc10\\lib\\opencv_core244.lib")  
+#pragma comment(lib,"C:\\private\\program\\opencv\\opencv\\build\\x86\\vc10\\lib\\opencv_imgproc244.lib")  
+#pragma comment(lib,"C:\\private\\program\\opencv\\opencv\\build\\x86\\vc10\\lib\\opencv_highgui244.lib")  
+#pragma comment(lib,"C:\\private\\program\\opencv\\opencv\\build\\x86\\vc10\\lib\\opencv_objdetect244.lib")
+#pragma comment(lib,"C:\\private\\program\\opencv\\opencv\\build\\x86\\vc10\\lib\\opencv_contrib244.lib")  
+#pragma comment(lib,"C:\\private\\program\\opencv\\opencv\\build\\x86\\vc10\\lib\\opencv_features2d244.lib") 
+#pragma comment(lib,"C:\\private\\program\\opencv\\opencv\\build\\x86\\vc10\\lib\\opencv_flann244.lib")  
+#pragma comment(lib,"C:\\private\\program\\opencv\\opencv\\build\\x86\\vc10\\lib\\opencv_gpu244.lib")   
+#pragma comment(lib,"C:\\private\\program\\opencv\\opencv\\build\\x86\\vc10\\lib\\opencv_haartraining_engined.lib")  
+#pragma comment(lib,"C:\\private\\program\\opencv\\opencv\\build\\x86\\vc10\\lib\\opencv_legacy244.lib")   
+#pragma comment(lib,"C:\\private\\program\\opencv\\opencv\\build\\x86\\vc10\\lib\\opencv_ts244.lib")   
+#pragma comment(lib,"C:\\private\\program\\opencv\\opencv\\build\\x86\\vc10\\lib\\opencv_video244.lib")
+#pragma comment(lib,"C:\\private\\program\\opencv\\opencv\\build\\x86\\vc10\\lib\\opencv_calib3d244.lib")
+#pragma comment(lib,"C:\\private\\program\\midi\\MIDIIOLib0.8\\Release\\MIDIIO.lib")
 #endif
+
+#pragma comment(lib,"C:\\private\\program\\glut\\glut-3.7.6-bin\\glut32.lib")
+
+
 using namespace cv;
 using namespace std;
 
